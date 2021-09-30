b147371a4baf4131eca6ce33927d0ef04e79f9a0
SIANI <l.siani92@gmail.com>
2021-09-30 15:46:58 +0200

    add project

---
+++ Assets/Editor/MovableObjectEditor.cs
@@ -0,0 +1,316 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+[CanEditMultipleObjects]
+[CustomEditor(typeof(MovableObject))]
+public class MovableObjectEditor : Editor
+{
+    private MovableObject component;
+    private SerializedProperty type;
+    private SerializedProperty isMovingOnStart;
+    private SerializedProperty isChangingDirection;
+    private SerializedProperty startingPos;
+    private SerializedProperty center;
+    private SerializedProperty demiCircle;
+    private SerializedProperty currentDist;
+    private SerializedProperty radius;
+    private SerializedProperty segments;
+    private SerializedProperty speed;
+    private SerializedProperty useCurvedSpeed;
+    private SerializedProperty curve;
+    private SerializedProperty loop;
+    private SerializedProperty close;
+    private SerializedProperty isReversed;
+    private SerializedProperty startMovement;
+    private SerializedProperty endMovement;
+    private bool oldClose;
+    private MovableObject.MovementType typeEnum;
+    private bool showPoints = false;
+    void OnAwake()
+    {
+        Initialize();
+    }
+
+    void OnEnable()
+    {
+        Initialize();
+        oldClose = close.boolValue;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        EditorGUI.BeginChangeCheck();
+
+        int oldSizeArray = segments.arraySize;
+        serializedObject.Update();
+        typeEnum = (MovableObject.MovementType)type.enumValueIndex;
+
+        EditorGUILayout.PropertyField(type);
+
+        EditorGUILayout.PropertyField(isMovingOnStart);
+        if (typeEnum != MovableObject.MovementType.PingPong)
+        {
+            EditorGUILayout.PropertyField(loop);
+        }
+        else
+        {
+            loop.boolValue = false;
+        }
+        
+        EditorGUILayout.PropertyField(close);
+        EditorGUILayout.PropertyField(isChangingDirection);
+
+        EditorGUILayout.PropertyField(startingPos);
+        EditorGUILayout.PropertyField(radius);
+
+        if (radius.floatValue > 0.0f)
+            EditorGUILayout.PropertyField(demiCircle);
+
+        int newsSize = EditorGUILayout.IntField("Number of points", segments.arraySize);
+
+        if (newsSize != segments.arraySize)
+            segments.arraySize = newsSize;
+
+        if (close.boolValue)
+        {
+            if (oldClose != close.boolValue)
+            {
+                segments.arraySize += 1;
+                oldClose = close.boolValue;
+            }
+        }
+
+        if (oldClose != close.boolValue)
+        {
+            segments.arraySize -= 1;
+            oldClose = close.boolValue;
+        }
+
+        if (radius.floatValue <= 0.0f)
+        {
+            showPoints = EditorGUILayout.Foldout(showPoints, "Points");
+            if (showPoints)
+            {
+
+                for (int i = 0; i < segments.arraySize; ++i)
+                {
+                    if (close.boolValue && i == segments.arraySize - 1)
+                        break;
+
+                    SerializedProperty item = segments.GetArrayElementAtIndex(i);
+                    SerializedProperty p1 = item.FindPropertyRelative("p1");
+                    SerializedProperty p2 = item.FindPropertyRelative("p2");
+                    if (i == 0)
+                    {
+                        p1.vector3Value = EditorGUILayout.Vector3Field("point " + i, p1.vector3Value);
+                        p2.vector3Value = EditorGUILayout.Vector3Field("point " + (i + 1), p2.vector3Value);
+                    }
+                    else
+                    {
+                        p2.vector3Value = EditorGUILayout.Vector3Field("point " + (i + 1), p2.vector3Value);
+                    }
+                }
+            }
+        }
+
+
+        EditorGUILayout.PropertyField(useCurvedSpeed);
+
+        if (useCurvedSpeed.boolValue)
+        {
+            EditorGUILayout.PropertyField(curve);
+        }
+        else
+        {
+            EditorGUILayout.PropertyField(speed);
+        }
+
+        EditorGUILayout.PropertyField(isReversed);
+        EditorGUILayout.PropertyField(startMovement);
+        EditorGUILayout.PropertyField(endMovement);
+
+        if (oldSizeArray == 0 && segments.arraySize != oldSizeArray)
+        {
+            for (int i = 0; i < segments.arraySize; i++)
+            {
+                SerializedProperty item = segments.GetArrayElementAtIndex(i);
+                SerializedProperty p1 = item.FindPropertyRelative("p1");
+                SerializedProperty p2 = item.FindPropertyRelative("p2");
+                p1.vector3Value = component.transform.position;
+                p2.vector3Value = component.transform.position;
+            }
+        }
+
+        serializedObject.ApplyModifiedProperties();
+
+        if (EditorGUI.EndChangeCheck() && !Application.isPlaying)
+        {
+            currentDist.floatValue = component.GetCurrentDistance(startingPos.floatValue);
+
+            if (isChangingDirection.boolValue)
+            {
+                component.transform.forward = component.CurrentDir;
+            }
+        }
+    }
+
+    private void OnSceneGUI()
+    {
+        EditorGUI.BeginChangeCheck();
+        if (segments.arraySize > 0)
+            Tools.current = Tool.None;
+
+        float radiusValue = radius.floatValue;
+
+        if (radiusValue <= 0.0f)
+        {
+            SetFreeSegments();
+        }
+        else
+        {
+            SetCircleSegments(radiusValue);
+        }
+        if (EditorGUI.EndChangeCheck() && !Application.isPlaying)
+        {
+            currentDist.floatValue = component.GetCurrentDistance(startingPos.floatValue);
+            Undo.RecordObject(target, "Changed Look Target");
+        }
+    }
+
+    private void SetFreeSegments()
+    {
+        Vector3 firstP = Vector3.zero;
+        Vector3 LastP = Vector3.zero;
+        Vector3 newP1 = Vector3.zero;
+        Vector3 newP2 = Vector3.zero;
+
+        float dist = 0.0f;
+
+        for (int i = 0; i < segments.arraySize; ++i)
+        {
+            if (i == 0)
+            {
+                newP1 = Handles.PositionHandle(component.Segments[i].p1, Quaternion.identity);
+                Handles.Label(newP1, i.ToString());
+
+                component.Segments[i].p1 = newP1;
+                firstP = newP1;
+            }
+            else
+            {
+                newP1 = LastP;
+                component.Segments[i].p1 = newP1;
+            }
+
+            if (close.boolValue && i == segments.arraySize - 1)
+            {
+                newP2 = firstP;
+            }
+            else
+            {
+                newP2 = Handles.PositionHandle(component.Segments[i].p2, Quaternion.identity);
+                Handles.Label(newP2, (i + 1).ToString());
+            }
+
+            component.Segments[i].p2 = newP2;
+
+            component.Segments[i].p1length = dist;
+
+            component.Segments[i].length = (newP2 - newP1).magnitude;
+
+            dist += component.Segments[i].length;
+
+            component.Segments[i].p2length = dist;
+
+            component.Segments[i].dir = (newP2 - newP1).normalized;
+
+            Handles.DrawDottedLine(newP1, newP2, 5.0f);
+
+            LastP = newP2;
+        }
+    }
+
+    private void SetCircleSegments(float _radius)
+    {
+        Vector3 firstP = Vector3.zero;
+        Vector3 LastP = Vector3.zero;
+        Vector3 newP1 = Vector3.zero;
+        Vector3 newP2 = Vector3.zero;
+        Vector3 newCenter = Vector3.zero;
+
+        float PI2 = Mathf.PI * ((demiCircle.boolValue) ? 1.0f : 2.0f);
+        float DivPI2 = PI2 / ((close.boolValue) ? segments.arraySize : (segments.arraySize + 1));
+
+        newCenter = Handles.PositionHandle(component.Center, Quaternion.identity);
+        component.Center = newCenter;
+
+        float dist = 0.0f;
+
+        for (int i = 0; i < segments.arraySize; ++i)
+        {
+            if (i == 0)
+            {
+                newP1 = newCenter + new Vector3(Mathf.Cos(DivPI2 * i), Mathf.Sin(DivPI2 * i)) * _radius;
+                Handles.Label(newP1, i.ToString());
+
+                component.Segments[i].p1 = newP1;
+                firstP = newP1;
+            }
+            else
+            {
+                newP1 = LastP;
+                component.Segments[i].p1 = newP1;
+            }
+
+            if (close.boolValue && i == segments.arraySize - 1)
+            {
+                newP2 = firstP;
+            }
+            else
+            {
+                newP2 = newCenter + new Vector3(Mathf.Cos(DivPI2 * (i + 1)), Mathf.Sin(DivPI2 * (i + 1))) * _radius;
+                Handles.Label(newP2, (i + 1).ToString());
+            }
+
+            component.Segments[i].p1length = dist;
+
+            component.Segments[i].length = (newP2 - newP1).magnitude;
+
+            component.Segments[i].p2 = newP2;
+
+            dist += component.Segments[i].length;
+
+            component.Segments[i].p2length = dist;
+
+            component.Segments[i].dir = (newP2 - newP1).normalized;
+
+            Handles.DrawDottedLine(newP1, newP2, 5.0f);
+
+            LastP = newP2;
+        }
+    }
+
+    private void Initialize()
+    {
+        component = (MovableObject)target;
+
+        type = serializedObject.FindProperty("type");
+        isMovingOnStart = serializedObject.FindProperty("isMovingOnStart");
+        isChangingDirection = serializedObject.FindProperty("isChangingDirection");
+        startingPos = serializedObject.FindProperty("startingPos");
+        currentDist = serializedObject.FindProperty("currentDist");
+        center = serializedObject.FindProperty("center");
+        demiCircle = serializedObject.FindProperty("demiCircle");
+        radius = serializedObject.FindProperty("radius");
+        segments = serializedObject.FindProperty("segments");
+        speed = serializedObject.FindProperty("speed");
+        useCurvedSpeed = serializedObject.FindProperty("useCurvedSpeed");
+        curve = serializedObject.FindProperty("curve");
+        isReversed = serializedObject.FindProperty("isReversed");
+        startMovement = serializedObject.FindProperty("startMovement");
+        endMovement = serializedObject.FindProperty("endMovement");
+        loop = serializedObject.FindProperty("loop");
+        close = serializedObject.FindProperty("close");
+    }
+}
---
+++ Assets/Runtime/MovableObject.cs
@@ -0,0 +1,311 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MovableObject : MonoBehaviour
+{
+    public enum MovementType
+    {
+        Linear,
+        PingPong
+    }
+
+    [System.Serializable]
+    public class Segment
+    {
+        public Vector3 p1;
+        public Vector3 p2;
+        public Vector3 dir;
+        public float length = 0.0f;
+        public float p1length = 0.0f;
+        public float p2length = 0.0f;
+    }
+#pragma warning disable 414
+    [SerializeField]
+    private UnityEvent startMovement = null;
+
+    [SerializeField]
+    private UnityEvent endMovement = null;
+
+    [SerializeField]
+    private MovementType type = MovementType.Linear;
+    [SerializeField]
+    private Segment[] segments = null;
+    [SerializeField]
+    private float speed = 5.0f;
+    [SerializeField]
+    private bool useCurvedSpeed = false;
+    [SerializeField]
+    private AnimationCurve curve = null;
+    [SerializeField]
+    private bool isReversed = false;
+    [SerializeField]
+    [Range(0.0f, 1.0f)]
+    private float startingPos = 0.0f;
+
+
+    [SerializeField]
+    private bool isMovingOnStart = true;
+    [SerializeField]
+    private bool isChangingDirection = false;
+    [SerializeField]
+    private bool loop = false;
+    [SerializeField]
+    private bool close = false;
+
+    [SerializeField]
+    private Vector3 center;
+    [SerializeField]
+    private bool demiCircle = false;
+
+    [SerializeField]
+    private float radius = 0.0f;
+    [SerializeField]
+    private float currentDist = 0.0f;
+
+#pragma warning restore 414
+    public delegate void OnUpdate();
+    public OnUpdate onUpdate;
+
+    private Vector3 currentDir = Vector3.zero;
+    private bool pingpong = false;
+    private float curvedSpeed = 0.0f;
+
+    public float CurrentDist { get => currentDist; }
+    public bool Pingpong { get => pingpong; set => pingpong = value; }
+    public Segment[] Segments { get => segments; set => segments = value; }
+    public float Speed
+    {
+        get
+        {
+            if (useCurvedSpeed)
+                return curvedSpeed;
+            else
+                return speed;
+        }
+        set => speed = value;
+    }
+    public Vector3 Center { get => center; set => center = value; }
+    public Vector3 CurrentDir
+    {
+        get
+        {
+            float sign = 1.0f;
+            if (isReversed)
+            {
+                sign = -1.0f;
+            }
+
+            if (pingpong)
+            {
+                sign *= -1.0f;
+            }
+            // Debug.Log(currentDir * sign);
+            return currentDir * sign;
+        }
+    }
+
+    private void Start()
+    {
+        if (isMovingOnStart && startMovement != null)
+        {
+            startMovement.Invoke();
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!isMovingOnStart) return;
+
+        switch (type)
+        {
+            case MovementType.Linear:
+                UpdateLinear();
+                break;
+            case MovementType.PingPong:
+                UpdatePingPong();
+                break;
+        }
+
+        if (isChangingDirection)
+            transform.forward = currentDir;
+    }
+
+    private void UpdateLinear()
+    {
+        if (!isReversed)
+        {
+            UpdatePositionASC(loop);
+        }
+        else
+        {
+            UpdatePositionDESC(loop);
+        }
+    }
+
+    private void UpdatePingPong()
+    {
+        if (!isReversed)
+        {
+            if (!pingpong)
+            {
+                UpdatePositionASC(loop);
+
+                if (currentDist > segments[(segments.Length - 1)].p2length)
+                    pingpong = true;
+            }
+            else
+            {
+                UpdatePositionDESC(loop);
+
+                if (currentDist < 0.0f)
+                    pingpong = false;
+            }
+        }
+        else
+        {
+            if (pingpong)
+            {
+                UpdatePositionASC(loop);
+
+                if (currentDist > segments[(segments.Length - 1)].p2length)
+                    pingpong = false;
+            }
+            else
+            {
+                UpdatePositionDESC(loop);
+
+                if (currentDist < 0.0f)
+                    pingpong = true;
+            }
+        }
+    }
+
+    private void UpdateLoop()
+    {
+        if (!isReversed)
+        {
+            UpdatePositionASC(true);
+        }
+        else
+        {
+            UpdatePositionDESC(true);
+        }
+    }
+
+    public void Play()
+    {
+        if (startMovement != null)
+        {
+            startMovement.Invoke();
+        }
+
+        isMovingOnStart = true;
+    }
+
+    public void Stop()
+    {
+        isMovingOnStart = false;
+    }
+
+    private Vector3 GetPosition(float _dist)
+    {
+        float dist = Mathf.Clamp(_dist, 0.0f, segments[segments.Length - 1].p2length);
+
+        for (int i = 0; i < segments.Length; ++i)
+        {
+            if (dist >= segments[i].p1length && dist <= segments[i].p2length)
+            {
+                if (segments[i].length == 0)
+                {
+                    continue;
+                }
+
+                float t = (dist - segments[i].p1length) / segments[i].length;
+                currentDir = segments[i].dir;
+                return Vector3.Lerp(segments[i].p1, segments[i].p2, t);
+            }
+        }
+        return transform.localPosition;
+    }
+
+    private void UpdatePositionASC(bool _islooping = false)
+    {
+        if (currentDist <= segments[segments.Length - 1].p2length)
+        {
+            if (onUpdate != null)
+            {
+                onUpdate();
+            }
+
+            if (useCurvedSpeed)
+            {
+                curvedSpeed = curve.Evaluate((currentDist / segments[segments.Length - 1].p2length));
+                currentDist += Time.fixedDeltaTime * curvedSpeed;
+            }
+            else currentDist += Time.fixedDeltaTime * speed;
+
+            if (currentDist > segments[segments.Length - 1].p2length)
+            {
+                if (_islooping)
+                {
+                    currentDist = 0.0f;
+                }
+
+                if (endMovement != null)
+                {
+                    endMovement.Invoke();
+                }
+            }
+
+            transform.localPosition = GetPosition(currentDist);
+        }
+    }
+
+    private void UpdatePositionDESC(bool _islooping = false)
+    {
+        if (currentDist >= 0.0f)
+        {
+            if (onUpdate != null)
+            {
+                onUpdate();
+            }
+
+            if (useCurvedSpeed)
+            {
+                curvedSpeed = curve.Evaluate(currentDist / segments[segments.Length - 1].p2length);
+                currentDist -= Time.fixedDeltaTime * curvedSpeed;
+            }
+            else currentDist -= Time.fixedDeltaTime * speed;
+
+            if (currentDist < 0.0f)
+            {
+                if (_islooping)
+                {
+                    currentDist = segments[segments.Length - 1].p2length;
+                }
+
+                if (endMovement != null)
+                {
+                    endMovement.Invoke();
+                }
+            }
+
+            transform.localPosition = GetPosition(currentDist);
+        }
+    }
+
+    public float GetCurrentDistance(float _time)
+    {
+        if (segments != null && segments.Length > 0)
+        {
+            float totalLength = segments[segments.Length - 1].p2length;
+            currentDist = _time * totalLength;
+            transform.localPosition = GetPosition(currentDist);
+            return currentDist;
+        }
+        else return currentDist;
+    }
+}
